b69c9b88671da6896463d665c386c8e8a96b369c
Mark Jeffri Dela Cruz <markjeffridelacruz@gmail.com>
2020-06-28 15:41:24 +0800

    Updates

---
+++ precache-manifest.e5bc24efb509e260f16343a6625ec17c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2412660fc400ef0905e73262feed6375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa10d020c47f8358d6ed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fed545af80d7e87e38e",
+    "url": "/static/js/2.f26d5fee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f26d5fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa10d020c47f8358d6ed",
+    "url": "/static/js/main.c67ce453.chunk.js"
+  },
+  {
+    "revision": "ba077e8f15e9a38726ff",
+    "url": "/static/js/runtime-main.875e6c2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
